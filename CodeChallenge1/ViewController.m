df6a8dbaf3d54f7e36754ad4f4afea5fae401b43
roshan <roshan.prakash.r@gmail.com>
2014-10-02 21:25:44 +0100

    layout issues fixed

--- CodeChallenge1/ViewController.m
+++ CodeChallenge1/ViewController.m
@@ -28,11 +28,6 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-
-
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -49,8 +44,6 @@
     {
         self.webBarButtonItem.enabled = YES;
     }
-
-
 
 }
 
